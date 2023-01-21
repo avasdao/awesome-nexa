56cb0124ed0beee214a836d1c8b648fd1baf32c5
Shomari <nyusternie@sdot.io>
2023-01-21 03:41:24 -0500

    Add team page.

---
+++ web/pages/Team.vue
@@ -0,0 +1,24 @@
+<script setup>
+/* Import modules. */
+import { ref } from 'vue'
+
+//
+</script>
+
+<template>
+    <main class="">
+        <HeaderView />
+
+        <div class="pt-10 flex justify-center">
+            <h1 class="text-5xl font-bold tracking-widest">
+                The Nexa Team
+            </h1>
+        </div>
+
+        <section class="py-10 flex flex-col items-center gap-10">
+            <p>
+                Andrew Stone, Paul Church &amp; Dr. Peter Rizun
+            </p>
+        </section>
+    </main>
+</template>
