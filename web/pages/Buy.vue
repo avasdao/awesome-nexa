6249032fa2d36606c66191d15a54e9e356012e91
Shomari <nyusternie@sdot.io>
2023-02-05 03:39:12 -0500

    Update Buy page.

--- web/pages/Buy.vue
+++ web/pages/Buy.vue
@@ -15,35 +15,159 @@ import { ref } from 'vue'
 
         <section class="py-10 flex flex-col items-center gap-10">
             <p>
+                Are you ready to own a piece of the future?
+            </p>
+
+            <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod, commodi id asperiores reiciendis temporibus nisi quasi dolorem perspiciatis unde atque illo? Sed porro similique debitis perferendis dolor sequi voluptas!
             </p>
         </section>
 
-<!-- THIS WILL BECOME A STANDALONE WIDGET HOSTED @ WIDGETS.NEXA.EXCHANGE -->
+        <section class="mx-10 my-10 px-7 py-3 bg-gradient-to-r from-indigo-100 to-indigo-300 border-4 border-indigo-700 rounded-2xl shadow">
 
-        <section class="mx-10 my-3 px-7 py-3 bg-gradient-to-r from-rose-200 to-rose-400 border-4 border-rose-700 rounded-full shadow">
-
-            <h1 class="text-4xl font-medium">
-                Buy Nexa
+            <h1 class="text-3xl font-medium">
+                <span class="text-2xl text-indigo-700">STEP #1:</span>
+                What is your Nexa wallet address?
             </h1>
 
+            <input
+                type="text"
+                placeholder="nexa:"
+                class="w-3/4 my-3 px-3 py-2 text-2xl text-indigo-500 font-medium border-2 border-indigo-500 rounded placeholder:text-indigo-300 placeholder:font-medium"
+            />
+
+            <p class="text-lg">
+                This is the <NuxtLink to="/wallets" class="text-blue-900 font-medium hover:underline">Wallet</NuxtLink> where you want your NEXA to be delivered after the swap completes.
+            </p>
+
+            <NuxtLink to="/wallets" class="my-3 block text-blue-900 font-medium hover:underline">
+                Do you need a Wallet?
+            </NuxtLink>
+        </section>
+
+        <section class="mx-10 my-10 px-7 py-3 bg-gradient-to-r from-indigo-100 to-indigo-300 border-4 border-indigo-700 rounded-2xl shadow">
+
+            <h1 class="text-3xl font-medium">
+                <span class="text-2xl text-indigo-700">STEP #2:</span>
+                How do you want to Pay?
+            </h1>
+
+            <ul class="pl-10 list-disc">
+                <li>Bitcoin (BTC)</li>
+                <li>Bitcoin Cash (BCH)</li>
+                <li>
+                    Tether (USDT)
+                    <ol class="pl-10 list-disc">
+                        <li>BNB Smart Chain (BSC)</li>
+                        <li>Ethereum (ETH)</li>
+                        <li>Tron (TRX)</li>
+                    </ol>
+                </li>
+            </ul>
+
             <div class="flex justify-end">
-                <h3 class="text-sm text-rose-900">
+                <h3 class="text-sm text-indigo-900">
                     Powered by <NuxtLink to="https://nexa.exchange" target="_blank" class="font-medium hover:underline">Nexa.Exchange</NuxtLink>
                 </h3>
             </div>
 
         </section>
 
-<!-- THIS WILL BECOME A STANDALONE WIDGET HOSTED @ WIDGETS.NEXA.EXCHANGE -->
+        <section class="mx-10 my-10 px-7 py-3 bg-gradient-to-r from-indigo-100 to-indigo-300 border-4 border-indigo-700 rounded-2xl shadow">
 
-        <section class="px-10 py-3">
-            <h3 class="text-lg">
-                Want to Sell Nexa?
+            <h1 class="text-3xl font-medium">
+                <span class="text-2xl text-indigo-700">STEP #3:</span>
+                How much do you want to Buy?
+            </h1>
+
+            <div class="mt-5 w-72">
+                <div class="flex justify-between items-end my-1 px-5 py-3 bg-gray-100 rounded-xl">
+                    <span class="text-base font-light">USD</span>
+
+                    <span class="text-3xl font-medium">$13.37</span>
+                </div>
+
+                <div class="px-5 flex justify-end">
+                    <span class="text-sm">
+                        Minimum purchase is <span class="text-lg font-medium text-indigo-700">$45.00</span>
+                        <span class="ml-1 text-xs text-indigo-500">USD</span>
+                    </span>
+                </div>
+
+                <div class="px-5 flex justify-end">
+                    <span class="text-sm">
+                        Maximum purchase is <span class="text-lg font-medium text-indigo-700">$1,000.00</span>
+                        <span class="ml-1 text-xs text-indigo-500">USD</span>
+                    </span>
+                </div>
+
+                <div class="mt-5 flex gap-4">
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        1
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        2
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        3
+                    </Button>
+                </div>
+
+                <div class="mt-5 flex gap-4">
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        4
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        5
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        6
+                    </Button>
+                </div>
+
+                <div class="mt-5 flex gap-4">
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        7
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        8
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        9
+                    </Button>
+                </div>
+
+                <div class="mt-5 flex gap-4">
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        .
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        0
+                    </Button>
+
+                    <Button class="w-1/3 h-16 p-2 bg-gray-500 rounded-xl hover:bg-gray-600">
+                        &lt;
+                    </Button>
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="flex flex-col items-end px-10 py-10">
+            <h3 class="text-xl">
+                Do you need to <span class="font-medium">Sell</span> some NEXA?
             </h3>
 
-            <h3 class="text-lg">
-                Visit <NuxtLink to="https://swap.nexa.exchange" target="_blank" class="text-blue-500 font-medium hover:underline">NexaSwap</NuxtLink>
+            <h3 class="text-xl">
+                Visit <NuxtLink to="https://swap.nexa.exchange" target="_blank" class="text-2xl text-blue-500 font-medium hover:underline">NexaSwap</NuxtLink> for instant exchange to <span class="font-medium">over 100+</span> Cryptos
             </h3>
         </section>
 
