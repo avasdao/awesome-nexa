ae2cd6e1ada068df9d0446130f449ca2be356115
Shomari <nyusternie@sdot.io>
2023-02-05 08:16:43 -0500

    Complete first listing.

--- web/layouts/listing.vue
+++ web/layouts/listing.vue
@@ -16,48 +16,37 @@ import { ref } from 'vue'
             </h1>
         </header>
 
-        <section class="py-2 flex flex-col items-center">
+        <section class="mt-5 mb-10 flex flex-col items-center">
             <slot name="url">
                 https://site-url.com/
             </slot>
         </section>
 
-        <section class="py-1 flex flex-col items-center">
-            <h3 class="text-2xl font-medium text-gray-500 italic">
-                <slot name="tagline">
-                    Enter a short tagline here
-                </slot>
-            </h3>
-        </section>
-
-        <section>
+        <section class="flex">
             <slot name="slideshow">
                 <img
                     src="~/assets/ani-banner.gif"
-                    class="my-10 w-7/12 h-96 border-4 border-indigo-500 rounded-xl shadow object-cover"
+                    class="my-0 w-7/12 h-96 border-4 border-indigo-500 rounded-xl shadow object-cover"
                 />
             </slot>
+
+            <div class="w-full px-5 pt-7">
+                <p class="text-2xl font-medium text-gray-500">
+                    <span class="inline-block relative -mr-2 text-7xl text-yellow-500">&ldquo;</span>
+                    <slot name="summary">
+                        Enter a short summary here
+                    </slot>
+                    <span class="inline-block relative -mt-10 text-7xl text-yellow-500">&bdquo;</span>
+                </p>
+            </div>
         </section>
 
-        <section class="my-5 px-10 flex flex-col items-center">
-            <p class="text-lg text-gray-500">
+        <section class="my-10 px-10 flex flex-col items-center">
+            <div class="flex flex-col gap-6 text-lg text-gray-500">
                 <slot name="description">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam harum quod laudantium praesentium doloribus error qui natus neque, voluptates expedita deleniti modi ducimus sequi quaerat ipsa fugit. Fugit, corrupti ad.
                 </slot>
-            </p>
-        </section>
-
-        <section>
-            <p class="w-1/2 my-5 px-7 py-5 text-sm bg-gradient-to-r from-rose-100 to-rose-200 border-2 border-rose-300 rounded-xl shadow">
-                Awesome Nexa is NOT responsible for ANY of the resources listed in this catalog.
-                Using this site, you take your own risks.
-
-                <span class="my-2 block text-base text-rose-700 text-center font-bold">DO YOUR OWN RESEARCH (DYOR)</span>
-
-                <span class="block">
-                    Read our <NuxtLink to="/disclaimer" class="text-rose-700 font-medium hover:underline">Disclaimer Terms</NuxtLink> and <NuxtLink to="/privacy" class="text-rose-700 font-medium hover:underline">Privacy Policy</NuxtLink>
-                </span>
-            </p>
+            </div>
         </section>
 
         <hr />
@@ -109,6 +98,21 @@ import { ref } from 'vue'
                     You must first <span class="px-2 py-1 bg-blue-500 text-white font-medium rounded">Get Connected</span> with a Nexa ID-compatible wallet to write a review.
                 </p>
             </div>
+        </section>
+
+        <hr />
+
+        <section class="my-10">
+            <p class="w-1/2 my-5 px-7 py-5 text-sm bg-gradient-to-r from-rose-100 to-rose-200 border-2 border-rose-300 rounded-xl shadow">
+                Awesome Nexa is NOT responsible for ANY of the resources listed in this catalog.
+                Using this site, you take your own risks.
+
+                <span class="my-2 block text-base text-rose-700 text-center font-bold">DO YOUR OWN RESEARCH (DYOR)</span>
+
+                <span class="block">
+                    Read our <NuxtLink to="/disclaimer" class="text-rose-700 font-medium hover:underline">Disclaimer Terms</NuxtLink> and <NuxtLink to="/privacy" class="text-rose-700 font-medium hover:underline">Privacy Policy</NuxtLink>
+                </span>
+            </p>
         </section>
     </main>
 </template>
--- web/pages/Mining/WoolyPooly.vue
+++ web/pages/Mining/WoolyPooly.vue
@@ -15,25 +15,37 @@ import { ref } from 'vue'
             <a
                 href="https://woolypooly.com/en/coin/nexa"
                 target="_blank"
-                class="text-xl text-blue-500 font-medium hover:underline"
+                class="flex gap-3 group items-center px-5 py-2 bg-blue-500 text-xl text-blue-50 font-medium rounded-2xl border-4 border-blue-300 hover:bg-blue-700 hover:border-yellow-400 hover:text-yellow-300"
             >
                 https://woolypooly.com/en/coin/nexa
+                <svg class="w-6 h-6 group-hover:text-yellow-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
+                </svg>
             </a>
-        </template>
-
-        <template #tagline>
-            An experienced mining pool
         </template>
 
         <template #slideshow>
             <img
                 src="~/assets/screenshots/wooly-pooly_01.jpg"
-                class="my-10 w-7/12 h-96 border-4 border-indigo-500 rounded-xl shadow object-cover"
+                class="my-0 w-7/12 h-96 border-4 border-indigo-500 rounded-xl shadow object-cover"
             />
         </template>
 
+        <template #summary>
+            An experienced mining pool for the Nexa community
+        </template>
+
         <template #description>
-            WoolyPooly is one of the most profitable pools with our focus on minimal commission fees for PPLNS & SOLO reward systems.
+            <p>
+                WoolyPooly is one of the most profitable pools with a focus on minimal commission fees for PPLNS & SOLO reward systems.
+            </p>
+
+            <p>
+                Nexa is mined using the NexaPow algorithm.
+                This is a core-dependent algorithm which can be mined in the same way as on Kaspa and Radiant (that is, -2000 for Hive OS and -1000 for Windows).
+                It is currently profitable to mine Nexa on GPU devices.
+                You can see the profitability of Nexa mining in their calculator.
+            </p>
         </template>
 
     </NuxtLayout>
