10432ef61be6e96e02f8e88303b4d9474ddb14a7
Shomari <nyusternie@sdot.io>
2023-02-06 00:14:05 -0500

    Add first banner to Hero.

--- web/components/HeroView.vue
+++ web/components/HeroView.vue
@@ -1,224 +1,49 @@
 <template>
-    <main class="overflow-hidden bg-gray-50 py-12 lg:py-16">
-        <div class="relative mx-auto max-w-xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <svg class="absolute left-full hidden -translate-x-1/2 -translate-y-1/4 transform lg:block" width="404" height="784" fill="none" viewBox="0 0 404 784" aria-hidden="true">
-                <defs>
-                    <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="784" fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
-            </svg>
+    <main class="max-w-7xl px-5 py-5 mx-auto flex flex-col lg:flex-row gap-4">
+        <section class="w-full lg:w-2/3">
 
-            <div class="my-5 flex justify-center">
-                <img src="~/assets/ani-banner.gif" class="w-8/12" />
-            </div>
+            <div class="h-96 p-5 mx-5 my-2 rounded-3xl bg-gray-900 border-4 border-yellow-500 shadow-lg">
 
-            <div class="relative">
-                <h2 class="text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-                    A Peer-to-Peer Digital Value System
+                <h2 class="relative text-gray-100 text-2xl font-medium z-10">
+                    Featured Spotlight
                 </h2>
 
-                <p class="mx-auto mt-4 max-w-3xl text-center text-xl text-gray-500">
-                    Nexa is a 3rd-generation Bitcoin blockchain which launched on June 21st 2022 and is a financial blockchain of programmable, packetized value.
-                </p>
-            </div>
+                <img
+                    src="~/assets/banners/txbit-listing.jpg"
+                    class="-mt-10 relative inset-0"
+                />
 
-            <div class="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
-                <div class="relative">
-                    <h3 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                        3rd Generation Bitcoin Blockchain
-                    </h3>
+                <div class="relative -mt-10 flex gap-6 justify-center">
+                    <button class="block w-4 h-4 bg-gray-200 rounded-full hover:bg-yellow-400" />
 
-                    <p class="mt-3 text-lg text-gray-500">
-                        Unlike Bitcoin which attempts to be a "Digital Gold", or Bitcoin Cash which attempts to be "Digital Cash", or Ethereum which attempts to be a "World Computer", <span class="font-medium">Nexa is a new blockchain which includes ALL the financial primitives required to build the NEW global, web3 economy.</span>
-                    </p>
+                    <button class="block w-4 h-4 bg-gray-200 rounded-full hover:bg-yellow-400" />
 
-                    <dl class="mt-10 space-y-10">
-                        <div class="relative">
-                            <dt>
-                                <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                                    <!-- Heroicon name: outline/globe-alt -->
-                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-                                        />
-                                    </svg>
-                                </div>
-
-                                <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
-                                    Scalable Up To 100K TPS
-                                </p>
-                            </dt>
-
-                            <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Nexa is taking layer-1 scalability to the next level. No other layers are necessary, meaning fees will stay low and there will always be capacity for your transactions and contracts.
-                            </dd>
-                        </div>
-
-                        <div class="relative">
-                            <dt>
-                                <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                                    <!-- Heroicon name: outline/scale -->
-                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z"
-                                        />
-                                    </svg>
-                                </div>
-
-                                <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
-                                    Native Tokens
-                                </p>
-                            </dt>
-
-                            <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Nexa is bringing advanced tokens & NFTs to layer-1. Create your new token product or service with no effort and essentially no cost due to the simple and efficient design.
-                            </dd>
-                        </div>
-
-                        <div class="relative">
-                            <dt>
-                                <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                                    <!-- Heroicon name: outline/bolt -->
-                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-                                    </svg>
-                                </div>
-
-                                <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
-                                    Meta/EVM Chain
-                                </p>
-                            </dt>
-
-                            <dd class="mt-2 ml-16 text-base text-gray-500">
-                                This EVM-compatible sidechain make it possible for Nexa uses to participate in advanced DeFi platforms.
-                            </dd>
-                        </div>
-                    </dl>
-                </div>
-
-                <div class="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
-                    <svg class="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404">
-                        <defs>
-                            <pattern id="ca9667ae-9f92-4be7-abcb-9e3d727f2941" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                            </pattern>
-                        </defs>
-                        <rect width="784" height="404" fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
-                    </svg>
-
-                    <section class="mx-3 sm:ml-10 pl-10 pr-5 pt-5 pb-2 bg-gradient-to-r from-yellow-50 to-yellow-100 border-2 border-yellow-200 rounded-lg shadow">
-                        <h1 class="text-2xl text-indigo-500 font-medium">
-                            Looking to Buy Nexa?
-                        </h1>
-
-                        <h3 class="text-gray-500">
-                            Choose from <strong>OVER 100+</strong> cryptos to <strong>INSTANTLY</strong> trade for $NEX
-                        </h3>
-
-                        <footer class="pt-5 flex justify-end">
-                            <a href="https://nexa.exchange" target="_blank" class="group text-xs text-gray-400 font-light">Powered by <span class="text-blue-400 font-normal group-hover:underline">Nexa.Exchange</span></a>
-                        </footer>
-                    </section>
-
-
-                    <img class="relative mx-auto" width="490" src="https://tailwindui.com/img/features/feature-example-1.png" alt="" />
+                    <button class="block w-4 h-4 bg-gray-200 rounded-full hover:bg-yellow-400" />
                 </div>
             </div>
 
-            <svg class="absolute right-full hidden translate-x-1/2 translate-y-12 transform lg:block" width="404" height="784" fill="none" viewBox="0 0 404 784" aria-hidden="true">
-                <defs>
-                    <pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="784" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-            </svg>
+        </section>
 
-            <div class="relative mt-12 sm:mt-16 lg:mt-24">
-                <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center lg:gap-8">
-                    <div class="lg:col-start-2">
-                        <h3 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                            New &amp; Noteworthy
-                        </h3>
+        <section class="w-full lg:w-1/3 px-3 py-5 flex flex-col gap-4 bg-rose-500">
 
-                        <p class="mt-3 text-lg text-gray-500">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex obcaecati natus eligendi delectus, cum deleniti sunt in labore nihil quod quibusdam expedita nemo.
-                        </p>
-
-                        <dl class="mt-10 space-y-10">
-                            <div class="relative">
-                                <dt>
-                                    <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                                        <!-- Heroicon name: outline/chat-bubble-bottom-center-text -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-                                            />
-                                        </svg>
-                                    </div>
-
-                                    <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
-                                        Monthly Showcase
-                                    </p>
-                                </dt>
-
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    On the 1st of every month, we send out an email newsletter highlighting the most notable events from the past month.
-                                </dd>
-                            </div>
-
-                            <div class="relative">
-                                <dt>
-                                    <div class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                                        <!-- Heroicon name: outline/envelope -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-                                            />
-                                        </svg>
-                                    </div>
-
-                                    <p class="ml-16 text-lg font-medium leading-6 text-gray-900">
-                                        Monthly BUIDL School
-                                    </p>
-                                </dt>
-
-                                <dd class="mt-2 ml-16 text-base text-gray-500">
-                                    Each month, we'll showcase a NEW and EXCITING feature of the Nexa blockchain for users to Learn, Code and Deliver into a real live decentralized application (dApp).
-                                </dd>
-                            </div>
-                        </dl>
-                    </div>
-
-                    <div class="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
-                        <svg class="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404" aria-hidden="true">
-                            <defs>
-                                <pattern id="e80155a9-dfde-425a-b5ea-1f6fadd20131" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                                    <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                                </pattern>
-                            </defs>
-                            <rect width="784" height="404" fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
-                        </svg>
-                        <img class="relative mx-auto" width="490" src="https://tailwindui.com/img/features/feature-example-2.png" alt="" />
-                    </div>
-                </div>
+            <div class="w-full px-3 py-1 bg-gray-900 rounded-lg">
+                <h2 class="text-gray-100 text-sm font-medium">
+                    Price / Chart
+                </h2>
             </div>
-        </div>
+
+            <div class="w-full px-3 py-1 bg-gray-900 rounded-lg">
+                <h2 class="text-gray-100 text-sm font-medium">
+                    Buy Nexa
+                </h2>
+            </div>
+
+            <div class="w-full px-3 py-1 bg-gray-900 rounded-lg">
+                <h2 class="text-gray-100 text-sm font-medium">
+                    Nexa News
+                </h2>
+            </div>
+
+        </section>
     </main>
 </template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
     <main>
-        <NewHeroView />
-        <!-- <HeroView /> -->
+        <HeroView />
 
         <GalleryView />
 
