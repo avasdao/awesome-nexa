88246ce5104c4f239dd6d27dc20b4761299e7f5d
Shomari <nyusternie@sdot.io>
2023-02-24 02:00:21 -0500

    Add CMC ticker manager to API.

--- web/server/routes/ticker.ts
+++ web/server/routes/ticker.ts
@@ -1,9 +1,49 @@
-export default defineEventHandler((event) => {
-    /* Set price. */
-    // FIXME FOR DEV PURPOSES ONLY
-    const price = 0.00001337
+const CMC_API_KEY: string | undefined = process.env.CMC_API_KEY
 
-    return {
-        price,
+/* Set endpoint. */
+const CMC_API_ENDPOINT: string = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+/* Set (request) target. */
+const target: string = CMC_API_ENDPOINT + `?symbol=NEXA` // ID = 23380
+
+/* Initialize ticker. */
+let ticker: Object | unknown
+
+const headers: HeadersInit = {
+    'accept': 'json',
+    'Content-type': 'application/json;charset=utf-8',
+    'X-CMC_PRO_API_KEY': CMC_API_KEY || '',
+}
+
+/**
+ * Manage the latest ticker information for NEXA.
+ *
+ * Will query CoinMarketCap API to retrieve the latest
+ * data every 300 seconds (5 minutes).
+ */
+;(async () => {
+    try {
+        /* Request data. */
+        const response: Object | unknown = await $fetch(target, {
+            headers,
+        })
+        console.log('API RESPONSE', response)
+
+        /* Set ticker to the response. */
+        ticker = response
+    } catch (err) {
+        // FIXME Handle errors. (setup Bugsnag)
+        console.error(err)
     }
+
+})()
+
+export default defineEventHandler((event) => {
+    /* Set error message. */
+    const errorMsg = {
+        error: `Oops! It looks like we don't have any ticker information. Administrators have been notified of the situation.`
+    }
+
+    /* Return ticker. */
+    return ticker?.data?.NEXA || errorMsg
 })
