e6f591c194d1ef177b8fc26ce12f60ddd69b27d2
Shomari <nyusternie@sdot.io>
2023-02-24 04:35:25 -0500

    Replace FAQ with Help.

--- web/components/HeaderView.vue
+++ web/components/HeaderView.vue
@@ -7,7 +7,7 @@ import { ref } from 'vue'
 const TICKER_UPDATE_INTERVAL = 60000 // 60 seconds
 
 /* Initialize flags. */
-const isShowingDownlaodsMenu = ref(false)
+// const isShowingDownlaodsMenu = ref(false)
 const isShowingExtrasMenu = ref(false)
 const isShowingMobileMenu = ref(false)
 
@@ -89,203 +89,11 @@ const connect = () => {
 
                 <div class="hidden md:flex md:flex-1 md:items-center md:justify-between">
                     <nav class="flex space-x-10 items-center">
-                        <div>
-                            <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
-                            <button
-                                @click="isShowingDownlaodsMenu = !isShowingDownlaodsMenu"
-                                type="button"
-                                class="px-2 py-1 text-gray-500 group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                aria-expanded="false"
-                            >
-                                <span>Downloads</span>
-                                <!--
-                    Heroicon name: mini/chevron-down
-
-                    Item active: "text-gray-600", Item inactive: "text-gray-400"
-                -->
-                                <svg class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-
-                            <!--
-                'Solutions' flyout menu, show/hide based on flyout menu state.
-
-                Entering: "transition ease-out duration-200"
-                    From: "opacity-0 -translate-y-1"
-                    To: "opacity-100 translate-y-0"
-                Leaving: "transition ease-in duration-150"
-                    From: "opacity-100 translate-y-0"
-                    To: "opacity-0 -translate-y-1"
-                -->
-                            <div v-if="isShowingDownlaodsMenu" class="absolute inset-x-0 top-full z-10 hidden transform bg-gradient-to-r from-yellow-100 to-yellow-200 shadow-lg md:block">
-                                <div class="mx-auto grid max-w-7xl gap-y-6 px-4 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12 xl:py-16">
-
-                                    <a href="https://www.nexa.org/download" target="_blank" class="-m-3 flex flex-col justify-between rounded-lg p-3 hover:bg-gray-50">
-                                        <div class="flex md:h-full lg:flex-col">
-                                            <div class="flex-shrink-0">
-                                                <span class="inline-flex h-10 w-10 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                                    <!-- Heroicon name: outline/chart-bar -->
-                                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                            </div>
-
-                                            <div class="ml-4 md:flex md:flex-1 md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                                <div>
-                                                    <p class="text-base font-medium text-gray-900">
-                                                        Download the Wallet
-                                                    </p>
-
-                                                    <p class="mt-1 text-sm text-gray-500">
-                                                        Install the desktop wallet from the "official" Nexa website to store your assets.
-                                                    </p>
-                                                </div>
-
-                                                <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">
-                                                    Learn more
-                                                    <span aria-hidden="true"> &rarr;</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a href="javascript://" class="-m-3 flex flex-col justify-between rounded-lg p-3 hover:bg-gray-50">
-                                        <div class="flex md:h-full lg:flex-col">
-                                            <div class="flex-shrink-0">
-                                                <span class="inline-flex h-10 w-10 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                                    <!-- Heroicon name: outline/cursor-arrow-rays -->
-                                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                            </div>
-                                            <div class="ml-4 md:flex md:flex-1 md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                                <div>
-                                                    <p class="text-base font-medium text-gray-900">Engagement</p>
-                                                    <p class="mt-1 text-sm text-gray-500">Speak directly to your customers in a more meaningful way.</p>
-                                                </div>
-                                                <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">
-                                                    Learn more
-                                                    <span aria-hidden="true"> &rarr;</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a href="javascript://" class="-m-3 flex flex-col justify-between rounded-lg p-3 hover:bg-gray-50">
-                                        <div class="flex md:h-full lg:flex-col">
-                                            <div class="flex-shrink-0">
-                                                <span class="inline-flex h-10 w-10 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                                    <!-- Heroicon name: outline/shield-check -->
-                                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                            </div>
-                                            <div class="ml-4 md:flex md:flex-1 md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                                <div>
-                                                    <p class="text-base font-medium text-gray-900">Security</p>
-                                                    <p class="mt-1 text-sm text-gray-500">Your customers&#039; data will be safe and secure.</p>
-                                                </div>
-                                                <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">
-                                                    Learn more
-                                                    <span aria-hidden="true"> &rarr;</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                    <a href="javascript://" class="-m-3 flex flex-col justify-between rounded-lg p-3 hover:bg-gray-50">
-                                        <div class="flex md:h-full lg:flex-col">
-                                            <div class="flex-shrink-0">
-                                                <span class="inline-flex h-10 w-10 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                                    <!-- Heroicon name: outline/squares-2x2 -->
-                                                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                            </div>
-                                            <div class="ml-4 md:flex md:flex-1 md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                                <div>
-                                                    <p class="text-base font-medium text-gray-900">Integrations</p>
-                                                    <p class="mt-1 text-sm text-gray-500">Connect with third-party tools that you&#039;re already using.</p>
-                                                </div>
-                                                <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">
-                                                    Learn more
-                                                    <span aria-hidden="true"> &rarr;</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-
-                                <div class="bg-gray-50">
-                                    <div class="mx-auto max-w-7xl space-y-6 px-4 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-6 lg:px-8">
-                                        <div class="flow-root">
-                                            <a href="javascript://" class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                <!-- Heroicon name: outline/play -->
-                                                <svg class="h-6 w-6 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-                                                </svg>
-                                                <span class="ml-3">Watch Demo</span>
-                                            </a>
-                                        </div>
-
-                                        <div class="flow-root">
-                                            <a href="javascript://" class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                <!-- Heroicon name: outline/check-circle -->
-                                                <svg class="h-6 w-6 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                                <span class="ml-3">View All Products</span>
-                                            </a>
-                                        </div>
-
-                                        <div class="flow-root">
-                                            <a href="javascript://" class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                <!-- Heroicon name: outline/phone -->
-                                                <svg class="h-6 w-6 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                                                    />
-                                                </svg>
-                                                <span class="ml-3">Contact Sales</span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <a href="https://github.com/avasdao/awesome-nexa#readme" target="_blank" class="text-base font-bold text-rose-500 hover:text-rose-700">
+                        <NuxtLink to="https://github.com/avasdao/awesome-nexa#readme" target="_blank" class="text-base font-bold text-rose-500 hover:text-rose-700">
                             <div class="flex">
                                 <img src="https://i.ibb.co/kq9gdS8/image.png" class="w-10" />
                                 Awesome List
                             </div>
-                        </a>
-
-                        <NuxtLink to="/faq" class="text-base font-medium text-gray-500 hover:text-gray-900">
-                            FAQ
                         </NuxtLink>
 
                         <div>
@@ -296,7 +104,7 @@ const connect = () => {
                                 class="px-2 py-1 text-gray-500 group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 aria-expanded="false"
                             >
-                                <span>What's Hot!</span>
+                                <span>🔥 Trending Now</span>
                                 <!--
                     Heroicon name: mini/chevron-down
 
@@ -519,6 +327,10 @@ const connect = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <NuxtLink to="/help" class="text-base font-medium text-gray-500 hover:text-gray-900">
+                            🙋 Need help?
+                        </NuxtLink>
                     </nav>
 
                     <div class="flex items-center md:ml-12">
