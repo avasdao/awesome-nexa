3cd09a97e0b708cfd5b4ee89cd5acb3542a927fb
Shomari <nyusternie@sdot.io>
2023-02-28 00:28:36 -0500

    Add (header) logs to db.

--- web/server/middleware/log.ts
+++ web/server/middleware/log.ts
@@ -1,4 +1,12 @@
-export default defineEventHandler((event) => {
+/* Import modules. */
+import moment from 'moment'
+import PouchDB from 'pouchdb'
+import { v4 as uuidv4 } from 'uuid'
+
+/* Initialize databases. */
+const logsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/logs`)
+
+export default defineEventHandler(async (event) => {
     /* Set headers. */
     const headers = event.node.req.headers
     console.log('HEADERS', headers)
@@ -9,6 +17,17 @@ export default defineEventHandler((event) => {
         client: headers['user-agent'],
         url: event.node.req.url,
     }
+
+    let success
+
+    success = await logsDb
+        .put({
+            _id: uuidv4(),
+            source: 'headers',
+            body: logDetails,
+            createdAt: moment().unix(),
+        })
+    console.log('SUCCESS (logs):', success)
 
     console.info('LOG (api):', logDetails)
 })
