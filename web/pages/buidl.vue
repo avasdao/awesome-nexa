d2cd0789626b87767c556d130d4e76dae71a1058
Shomari <nyusternie@sdot.io>
2024-06-04 01:51:39 -0400

    Add EPIC event page.

--- web/pages/buidl.vue
+++ web/pages/buidl.vue
@@ -13,10 +13,126 @@
             </h1>
         </div>
 
-        <section class="py-10 flex flex-col items-center gap-10">
+        <section class="py-10 flex flex-col gap-7 text-lg">
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod, commodi id asperiores reiciendis temporibus nisi quasi dolorem perspiciatis unde atque illo? Sed porro similique debitis perferendis dolor sequi voluptas!
+                BUIDL is a warping of the word “build” in the same fashion as “HODL.” BUIDL is a call to arms for building and contributing to the blockchain and cryptocurrency ecosystem, instead of passively holding.
             </p>
+
+            <p>
+                The BUIDL movement believes that instead of just accumulating or trading cryptocurrencies, people should start contributing proactively in order to help adoption and improve the ecosystem people invested.
+            </p>
+
+            <p>
+                The first thing that comes to mind when talking about building in a highly technological space such as the crypto space is that you need to be an expert in programming to contribute, but this is not the case for BUIDLing.
+            </p>
+
+            <h3 class="text-xl font-medium">
+                BUIDLing can involve simply using a cryptocurrency for it's intended purpose:
+            </h3>
+
+            <ol class="-mt-5 pl-10 list-disc">
+                <li>utilizing smart contracts</li>
+                <li>beta testing products</li>
+                <li>writing articles</li>
+                <li>playing blockchain games</li>
+                <li>using cryptocurrency wallets</li>
+                <li>and anything that could help the blockchain and cryptocurrency fields evolve and expand</li>
+            </ol>
+        </section>
+
+        <section class="py-10 flex flex-col gap-7 text-lg">
+            <h2 class="text-4xl font-bold tracking-widest">
+                #BUIDL Week 2024
+            </h2>
+
+            <h3 class="-mt-5 text-2xl text-gray-500 font-medium tracking-widest">
+                Monday, June <span class="text-rose-500 text-3xl font-bold">17th</span> thru Friday, June <span class="text-rose-500 text-3xl font-bold">21st</span>
+            </h3>
+
+            <p>
+                Monday (6/17) - Welcome &amp; Orientation
+            </p>
+
+            <p>
+                Tuesday (6/18) - TBD...
+            </p>
+
+            <p>
+                Wednesday (6/19) - TBD...
+            </p>
+
+            <p>
+                Thursday (6/20) - TBD...
+            </p>
+
+            <p>
+                Friday (6/21) - TBD...
+            </p>
+        </section>
+
+        <section class="py-10 flex flex-col gap-7 text-lg">
+            <h2 class="text-4xl font-bold tracking-widest">
+                EPIC Nexa Weekend
+            </h2>
+
+            <div class="grid grid-cols-2 gap-4">
+                <div class="-mt-5 text-xl text-gray-500 font-medium tracking-widest">
+                    <h4 class="text-base uppercase">
+                        Event Starts
+                    </h4>
+
+                    Friday, June <span class="text-rose-500 text-3xl font-bold">21st</span> @ 12:00 UTC
+                </div>
+
+                <div class="-mt-5 text-xl text-gray-500 font-medium tracking-widest">
+                    <h4 class="text-base uppercase">
+                        Submission Deadline
+                    </h4>
+
+                    Sudnay, June <span class="text-rose-500 text-3xl font-bold">23rd</span> @ 12:00 UTC
+                </div>
+            </div>
+
+            <div class="text-gray-500">
+                <h3>Event Notes</h3>
+
+                <ul class="pl-10 text-sm list-disc">
+                    <li>
+                        All participants MUST register ahead of time for $EPIC24 tokens for voting.
+                    </li>
+                </ul>
+            </div>
+
+            <div class="grid grid-cols-2 gap-4">
+                <div>
+                    <p>
+                        Non-technical Tracks (10M NEXA)
+                    </p>
+
+                    <ol class="pl-10 list-decimal">
+                        <li>Most EPIC Social Media Post</li>
+                        <li>Most EPIC Blog Post</li>
+                        <li>Most EPIC Event Meme</li>
+                        <li>Most EPIC Event Video</li>
+                        <li>Most EPIC Wiki Contribution(s)</li>
+                        <li>Most EPIC Bug Discovery</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <p>
+                        Technical Tracks (10M NEXA)
+                    </p>
+
+                    <ol class="pl-10 list-decimal">
+                        <li>Most EPIC Token Design</li>
+                        <li>Most EPIC Token Airdrop</li>
+                        <li>Most EPIC Dev Tutorial</li>
+                        <li>Most EPIC Nexscript Contract</li>
+                        <li>Most EPIC Developer Tool</li>
+                    </ol>
+                </div>
+            </div>
         </section>
     </main>
 </template>
---
+++ web/pages/epic/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+/* Import modules. */
+
+
+//
+</script>
+
+<template>
+    <main class="px-3 max-w-5xl mx-auto">
+        <div class="pt-10 flex justify-center">
+            <h1 class="text-5xl font-bold tracking-widest">
+                EPIC Nexa Weekend 2024
+            </h1>
+        </div>
+
+        <section class="py-10 flex flex-col items-center gap-10">
+            <p>
+                Celebrate the 2-YEAR ANNIVERSARY of Nexa by having fun building something Awesome with friends.
+            </p>
+        </section>
+    </main>
+</template>
