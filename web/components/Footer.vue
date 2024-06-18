ee7ef893865f2a39e32410bbc1158b4af1f2a6ec
Shomari <nyusternie@sdot.io>
2024-06-18 13:22:45 -0400

    Disable mailchimp.

--- web/components/Footer.vue
+++ web/components/Footer.vue
@@ -186,6 +186,7 @@
                             </button>
                         </div>
                     </form>
+
                 </div>
             </div>
 
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
             ],
             script: [
                 { src: '/js/matomo.js' },
-                { src: '/js/mailchimp.js' },
+                // { src: '/js/mailchimp.js' },
             ],
         },
     },
