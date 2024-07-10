cc669a9cbbfe097a02e81bce20fef7df5c3d5a97
Shomari <nyusternie@sdot.io>
2024-07-10 00:42:39 -0400

    Migrate to CouchDB for data storage.

---
+++ web/pages/v1.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+useHead({
+    title: `API Homepage — Awesome Nexa`,
+    meta: [
+        { name: 'description', content: `Awesome Nexa API is a FREE data resource for Builders.` }
+    ],
+})
+
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+const System = useSystemStore()
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
+
+<template>
+    <main class="max-w-5xl mx-auto py-5 flex flex-col gap-6">
+        <h1 class="text-5xl font-medium">
+            Awesome Nexa API Docs (v1)
+        </h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
+        </p>
+    </main>
+
+    <Footer />
+</template>
---
+++ web/server/routes/v1/listings/[category].get.ts
@@ -0,0 +1,56 @@
+/* Import modules. */
+import PouchDB from 'pouchdb'
+
+/* Initialize databases. */
+const listingsDb = new PouchDB(`https://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@db.awesomenexa.org/listings`)
+
+/*
+ * Import listings database.
+ */
+// import listingsDb from '../../../static/db/listings/listings.json'
+
+/* Initialize listings. */
+// const listings: Object[] = [...listingsDb]
+
+export default defineEventHandler(async _event => {
+    /* Initialzie locals. */
+    let listings
+    let sanitized
+
+    /* Set category. */
+    const category = _event?.context?.params?.category
+    console.log('CATEGORY', category)
+
+    /* Validate category. */
+    if (!category) {
+        return {
+            error: 'You MUST provide a category. (see the API Docs -- https://awesomenexa.org/v1)'
+        }
+    }
+
+    /* Request listings. */
+    listings = await listingsDb
+        .query('api/byCategory', {
+            key: category,
+            include_docs: true,
+        })
+        .catch(err => console.error(err))
+    console.log('LISTINGS', listings)
+
+    /* Sanitize listings. */
+    sanitized = listings.rows.map(_listing => {
+        /* Set document. */
+        const doc = _listing.doc
+
+        /* Remove metadata. */
+        delete doc._id
+        delete doc._rev
+
+        /* Return document. */
+        return doc
+    })
+    console.log('SANITIZED', sanitized)
+
+    /* Return listings. */
+    return sanitized
+})
