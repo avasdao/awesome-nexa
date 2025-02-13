2d408591d275bcb38824189c7c05b4bfb4cd1452
Shomari <nyusternie@sdot.io>
2025-02-13 14:22:56 -0500

    Update analytics.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,9 +14,9 @@ export default defineNuxtConfig({
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
             ],
-            // script: [
-            //     { src: '/js/matomo.js' },
-            // ],
+            script: [
+                { src: 'https://scripts.simpleanalyticscdn.com/latest.js' },
+            ],
         },
     },
 
