4780c373ba432bde3c35da74b0950060d034759b
Shomari <nyusternie@sdot.io>
2025-02-13 05:20:29 -0500

    Update wallets.

--- pages/wallets.vue
+++ pages/wallets.vue
@@ -25,6 +25,23 @@ const System = useSystemStore()
             Wallets are the life-blood of the Nexa ecosystem.
         </template>
 
+        <section>
+            <h2 class="text-3xl font-medium">
+                Rich List
+            </h2>
+
+            <ul>
+                <li>Plankton: 10K - 100K</li>
+                <li>Shrimp: 100K - 1M</li>
+                <li>Crab: 1M - 10M</li>
+                <li>Fish: 10M - 100M</li>
+                <li>Dolphin: 100M - 1B</li>
+                <li>Shark: 1B - 10B</li>
+                <li>Whale: 10B - 100B</li>
+                <li>Aquaman: 100B+</li>
+            </ul>
+        </section>
+
         <Gallery cat="wallets" :showAll="true" />
     </NuxtLayout>
 </template>
