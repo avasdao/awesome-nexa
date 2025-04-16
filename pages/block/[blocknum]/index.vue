dcd9ee4bfbb33d65ef765bbd059337469ba97e92
Shomari <nyusternie@sdot.io>
2025-04-16 05:07:46 -0400

    Add block nav buttons.

--- pages/block/[blocknum]/index.vue
+++ pages/block/[blocknum]/index.vue
@@ -11,7 +11,7 @@ const route = useRoute()
 // console.log('ROUTE PARAMS', route.params)
 
 /* Set block number. */
-const blocknum = route.params.blocknum
+const blocknum = parseInt(route.params.blocknum)
 
 /* Initialize handlers. */
 const block = ref(null)
@@ -74,6 +74,16 @@ onMounted(() => {
             <h1 class="text-4xl font-medium">
                 Block # {{blocknum}}
             </h1>
+
+            <section class="flex gap-4">
+                <NuxtLink :to="'/block/' + (blocknum - 1)" class="px-3 py-2 text-sm border border-slate-200 bg-slate-100 hover:bg-sky-100 rounded shadow">
+                    Go to Block # {{(blocknum - 1)}}
+                </NuxtLink>
+
+                <NuxtLink :to="'/block/' + (blocknum + 1)" class="px-3 py-2 text-sm border border-slate-200 bg-slate-100 hover:bg-sky-100 rounded shadow">
+                    Go to Block # {{(blocknum + 1)}}
+                </NuxtLink>
+            </section>
 
             <section class="grid grid-cols-4 gap-4">
                 <!-- <BlockItem
