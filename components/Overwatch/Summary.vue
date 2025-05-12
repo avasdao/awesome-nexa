c92921d08e6181ca9c136f2a6e05cde6e7132013
Shomari <nyusternie@sdot.io>
2025-05-12 14:26:29 -0400

    Remove header.

--- components/Overwatch/Summary.vue
+++ components/Overwatch/Summary.vue
@@ -1,49 +1,4 @@
 <template>
-    <header class="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-gray-900/10">
-        <div class="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-1 items-center gap-x-6">
-                <button type="button" class="-m-3 p-3 md:hidden" @click="mobileMenuOpen = true">
-                    <span class="sr-only">Open main menu</span>
-                    <Bars3Icon class="size-5 text-gray-900" aria-hidden="true" />
-                </button>
-                <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-            </div>
-            <nav class="hidden md:flex md:gap-x-11 md:text-sm/6 md:font-semibold md:text-gray-700">
-                <a v-for="(item, itemIdx) in navigation" :key="itemIdx" :href="item.href">{{ item.name }}</a>
-            </nav>
-            <div class="flex flex-1 items-center justify-end gap-x-8">
-                <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">View notifications</span>
-                    <BellIcon class="size-6" aria-hidden="true" />
-                </button>
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your profile</span>
-                    <img class="size-8 rounded-full bg-gray-800" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </a>
-            </div>
-        </div>
-        <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-            <div class="fixed inset-0 z-50" />
-            <DialogPanel class="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-white px-4 pb-6 sm:max-w-sm sm:px-6 sm:ring-1 sm:ring-gray-900/10">
-                <div class="-ml-0.5 flex h-16 items-center gap-x-6">
-                    <button type="button" class="-m-2.5 p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-                        <span class="sr-only">Close menu</span>
-                        <XMarkIcon class="size-6" aria-hidden="true" />
-                    </button>
-                    <div class="-ml-0.5">
-                        <a href="#" class="-m-1.5 block p-1.5">
-                            <span class="sr-only">Your Company</span>
-                            <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-                        </a>
-                    </div>
-                </div>
-                <div class="mt-2 space-y-2">
-                    <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
-                </div>
-            </DialogPanel>
-        </Dialog>
-    </header>
-
     <main>
         <div class="relative isolate overflow-hidden pt-16">
             <!-- Secondary navigation -->
