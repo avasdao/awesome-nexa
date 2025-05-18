ecd99b463973b83fcca5bcd4be3339958a416527
Shomari <nyusternie@sdot.io>
2025-05-18 03:31:49 -0400

    Rename edu program (as an extension of official).

--- pages/why.vue
+++ pages/why.vue
@@ -11,7 +11,7 @@
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:mx-0">
                 <p class="pl-3 text-base/7 font-semibold text-fuchsia-600 tracking-widest">
-                    Awesome Nexa University
+                    Awesome Nexa Academy
                 </p>
 
                 <h1 class="mt-2 text-pretty text-7xl font-light italic tracking-tight text-stone-500 sm:text-8xl">
